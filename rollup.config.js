089b53549939dad4123bd87c94b9df981b65f110
Simen Brekken <simen.brekken@nrk.no>
2020-04-16 09:45:28 +0200

    Even less

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
 export default {
   input: 'src/client/index.js',
   output: {
     file: 'build/client.js',
     format: 'cjs',
   },
+  plugins: [resolve(), commonjs()],
 };
